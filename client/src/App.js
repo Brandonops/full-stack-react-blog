f3d81783dc6396e56881c2f9fa877f629579be70
Lachlan Heywood <lachlan@heywood.net.au>
2021-04-12 10:27:16 -0400

    add user login

--- client/src/App.js
+++ client/src/App.js
@@ -8,27 +8,50 @@ import {
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import {
+  AppBar,
+  Button,
+  Container,
+  IconButton,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
+import Menu from '@material-ui/icons/Menu';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <nav>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/login">Login</NavLink>
-          <NavLink to="/register">Register</NavLink>
-        </nav>
-        <Switch>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-        </Switch>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <Menu />
+            </IconButton>
+            <Typography variant="h6" style={{ marginRight: 'auto' }}>
+              Full Stack Blog
+            </Typography>
+            <Button color="inherit" component={NavLink} to="/login">
+              Login
+            </Button>
+            <Button color="inherit" component={NavLink} to="/register">
+              Register
+            </Button>
+          </Toolbar>
+        </AppBar>
+
+        <Container style={{ margin: '2em auto' }}>
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+          </Switch>
+        </Container>
       </Router>
     </div>
   );
